8da868ad8c50a620c8e8b2ac1190d952a362488f
Jose Seie <joseeugenioseie@hotmail.com>
2018-06-08 05:37:04 +0200

    Update instalation script.sh

--- instalation script.sh
+++ instalation script.sh
@@ -23,3 +23,26 @@ echo "ServerName $servername" >> /etc/apache2/apache2.conf
 
 # Reiniciando apache
 sudo systemctl restart apache2
+
+# Permitindo todo o trafego ao APache full
+sudo ufw allow in "Apache Full"
+
+# Instalacao do mysql
+sudo apt-get install mysql-server
+
+# Instalacao de php
+apt-get update && apt-get upgrade
+
+# Instalando softwares de 3ros para a instalacao do php 7.2
+apt-get install python-software-properties
+add-apt-repository ppa:ondrej/php
+apt-get update
+
+# Instalando o php 7.2
+apt-get install php7.2
+
+# Instalando os modulos
+apt-get install php-pear php7.2-curl php7.2-dev php7.2-gd php7.2-mbstring php7.2-zip php7.2-mysql php7.2-xml
+
+
+
